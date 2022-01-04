d597f2b6cfa6399896518b340b241384b9795725
mitsui-h <mitsui-h@kanko.cityis.co.jp>
2022-01-04 18:17:32 +0900

    2022/01/04

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");// corsポリシーに抵触するため、その対策としてcorsを利用する
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
+const cors = require("cors"); // corsポリシーに抵触するため、その対策としてcorsを利用する
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 var app = express();
 var http = require("http").Server(app);
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 50001;
 //すべてのAPIをCORS許可したい場合
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -24,37 +24,39 @@ app.use("/public", express.static("public"));
 // app.use(express.static('files'));
 
 //セッション情報
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie:{
-  httpOnly: true,
-  secure: false,
-  maxage: 1000 * 60 * 30
-  }
-})); 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxage: 1000 * 60 * 1, //タイムアウト1分
+    },
+  })
+);
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    res.status(404);
-    res.end('Not found! : ' + req.path);
+app.use(function (req, res, next) {
+  res.status(404);
+  res.end("Not found! : " + req.path);
 });
 
-app.use(function(err, req, res, next){
-    res.status(500);
-    res.end('my 500 error! : ' + err);
+app.use(function (err, req, res, next) {
+  res.status(500);
+  res.end("my 500 error! : " + err);
 });
 
-io.on("connection", function(socket) {
-  socket.on("message", function(msg) {
+io.on("connection", function (socket) {
+  socket.on("message", function (msg) {
     console.log("message: " + msg);
     io.emit("message", msg);
   });
 });
 
-http.listen(PORT, function() {
+http.listen(PORT, function () {
   console.log("server listening. Port:" + PORT);
 });
