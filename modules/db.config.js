044bf002a9548ed765d5fce21f19798fe2273543
mitsui-h <mitsui-h@kanko.cityis.co.jp>
2022-02-04 22:18:59 +0900

    2022/02/04

--- modules/db.config.js
+++ modules/db.config.js
@@ -1,23 +1,44 @@
 exports.dbConf = [
-    {
-        host: 'harima-isk',
-        database: 'kc28382_bunkazai',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-    },
-    {
-        host: 'harima-isk',
-        database: 'kc28382_isk_cityisdb',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-    },
-    {
-        host: 'localhost',
-        database: 'db3',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-    }
-]
+  {
+    host: "harima-isk",
+    database: "kc28382_bunkazai",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+  {
+    host: "harima-isk",
+    database: "kc28382_isk_cityisdb",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+  {
+    host: "LG2201001",
+    database: "kc28382_bunkazai",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+  {
+    host: "LG2201001",
+    database: "kc28382_isk_cityisdb",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+  {
+    host: "localhost",
+    database: "kc28382_bunkazai",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+  {
+    host: "localhost",
+    database: "kc28382_isk_cityisdb",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+  },
+];
